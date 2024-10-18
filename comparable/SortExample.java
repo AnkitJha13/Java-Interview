6d34493150f2321cf98bfbf249ab5b8495cea966
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-10-18 23:31:10 +0530

    Add files via upload

---
+++ comparable/SortExample.java
@@ -0,0 +1,21 @@
+package comparable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortExample {
+        public static void main(String[] args) {
+            ArrayList<Emp> emps = new ArrayList<>();
+            emps.add(new Emp("athena", "8272837383", 2)); 
+            emps.add(new Emp("mamba", "7272835433", 1));
+            emps.add(new Emp("levinho", "9272837383", 3));
+
+            System.out.println(emps);
+
+            Collections.sort(emps);
+
+            System.out.println(emps + " after sorting");
+
+
+        }
+}
