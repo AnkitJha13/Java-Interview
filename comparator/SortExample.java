e5def9698958f9cc1b3e68dc861d460f32afe01e
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-10-18 23:31:35 +0530

    Add files via upload

---
+++ comparator/SortExample.java
@@ -0,0 +1,29 @@
+package comparator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SortExample {
+        public static void main(String[] args) {
+            ArrayList<Emp> emps = new ArrayList<>();
+            emps.add(new Emp("athena", "8272837383", 2)); 
+            emps.add(new Emp("mamba", "7272835433", 1));
+            emps.add(new Emp("levinho", "9272837383", 3));
+
+            System.out.println(emps);
+
+            Collections.sort(emps, new IdComparator());
+
+            System.out.println(emps + " after sorting based on ID");
+
+
+            ArrayList<Emp> emp1 = new ArrayList<>(emps);
+
+            Collections.sort(emp1, new NameComparator());
+
+            System.out.println(emp1 + " after sorting based on name");
+
+
+        }
+}
