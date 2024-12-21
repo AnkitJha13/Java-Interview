5dcde9a74000e57fb455dc751f3a1fb12d502874
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-12-21 12:46:55 +0530

    Create StringsSortingAscAndDesc.java

---
+++ StringsSortingAscAndDesc.java
@@ -0,0 +1,33 @@
+package com.athena.java_practice2.collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class StringsSortingAscAndDesc {
+
+    public static void main(String[] args) {
+
+        List<String> fruits = Arrays.asList("Mamba", "Athena", "Levinho", "Immortal");
+
+
+        List<String> ascending = fruits.stream()
+                .sorted()
+                .toList();
+
+        List<String> descending = fruits.stream()
+                .sorted(Collections.reverseOrder())
+                .toList();
+
+
+        System.out.println("Ascending order ---> " + ascending);
+
+        System.out.println("Descending order ---> " + descending);
+    }
+}
+
+
+// Output
+
+Ascending order ---> [Athena, Immortal, Levinho, Mamba]
+Descending order ---> [Mamba, Levinho, Immortal, Athena]
