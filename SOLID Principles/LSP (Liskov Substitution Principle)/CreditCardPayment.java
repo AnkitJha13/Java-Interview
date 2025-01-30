1d5535e951851c375466b68cbe9c9dad0510b6db
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-01-30 12:31:40 +0530

    Create CreditCardPayment.java

---
+++ SOLID Principles/LSP (Liskov Substitution Principle)/CreditCardPayment.java
@@ -0,0 +1,8 @@
+package lsp;
+
+public class CreditCardPayment extends Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing a credit card payment of $" + amount);
+    }
+}
