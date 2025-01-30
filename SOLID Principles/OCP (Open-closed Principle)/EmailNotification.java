2cf77afccad3be9af680eddb040ec21d11fc62e7
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-01-30 12:19:27 +0530

    Create EmailNotification.java

---
+++ SOLID Principles/OCP (Open-closed Principle)/EmailNotification.java
@@ -0,0 +1,10 @@
+package ocp;
+
+public class EmailNotification implements Notification {
+    public EmailNotification() {
+    }
+
+    public void send(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
