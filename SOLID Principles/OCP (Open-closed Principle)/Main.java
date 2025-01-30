051b0a240cc513e8cb38a3a11cb6de02b9b5b3b8
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-01-30 12:23:48 +0530

    Update Main.java

--- SOLID Principles/OCP (Open-closed Principle)/Main.java
+++ SOLID Principles/OCP (Open-closed Principle)/Main.java
@@ -1,18 +1,19 @@
 package ocp;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
+        // Using Email Notification
         Notification emailNotification = new EmailNotification();
         NotificationService emailService = new NotificationService(emailNotification);
         emailService.sendNotification("Welcome to our service!");
+
+        // Using SMS Notification
         Notification smsNotification = new SMSNotification();
         NotificationService smsService = new NotificationService(smsNotification);
         smsService.sendNotification("Your OTP is 123456");
     }
 }
+
 
 // Output
 
