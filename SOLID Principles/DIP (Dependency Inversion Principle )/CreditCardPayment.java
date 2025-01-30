a083f7e34b972e7d25daebc81a229ac48a6dc2e7
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-01-30 12:49:32 +0530

    Create CreditCardPayment.java

---
+++ SOLID Principles/DIP (Dependency Inversion Principle )/CreditCardPayment.java
@@ -0,0 +1,8 @@
+package dip;
+
+public class CreditCardPayment implements Payment {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of: $" + amount);
+    }
+}
