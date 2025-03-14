6420dffffd4dd921c1a6eb5c9e5a6a673b415633
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-14 18:07:21 +0530

    Update FullTimeEmployee.java

--- EmpManagementSystem/FullTimeEmployee.java
+++ EmpManagementSystem/FullTimeEmployee.java
@@ -1,15 +1,21 @@
+package EmpManagementSystem;
+
 public class FullTimeEmployee extends Employee{
 
     public double salary;
+    public  static double totalFullTimeSalary = 0; // ✅ Total salary for full-time
+
+    // constructor using super keyword
     public FullTimeEmployee( int id, String name, String department, double salary) {
         super(id, name, department);
         this.salary = salary;
+        totalFullTimeSalary += this.salary; // Adding salary to total using this
     }
 
-    @Override
+    @Override  // Override display method
     public void displayDetails(){
         super.displayDetails();
-        System.out.println("Employee Type: Full-time");
+        System.out.println("EmpManagementSystem.Employee Type: Full-time");
         System.out.println("Salary: $" + salary);
         System.out.println("------------------------");
     }
