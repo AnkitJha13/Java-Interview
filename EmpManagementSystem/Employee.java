79becc790bd4e92fd1198c2305325679fa31bc0c
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-14 18:07:03 +0530

    Update Employee.java

--- EmpManagementSystem/Employee.java
+++ EmpManagementSystem/Employee.java
@@ -1,14 +1,20 @@
+package EmpManagementSystem;
+
 public class Employee {
 
     public int id;
     public String name;
     public String department;
+    public static int totalEmployees;
+
+
 
     public Employee(int id, String name,  String department) {
 
         this.id = id;
         this.name = name;
         this.department = department;
+        totalEmployees++;
     }
 
     public void displayDetails(){
