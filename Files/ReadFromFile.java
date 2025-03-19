8684ec566e71ac7aec77c37a5b12980ec71da56f
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-19 21:30:10 +0530

    Update ReadFromFile.java

--- Files/ReadFromFile.java
+++ Files/ReadFromFile.java
@@ -21,13 +21,19 @@ public class ReadFromFile {
 }
 
 
+
 // Output
 - now this file is used to read data from file and will be printed on the screen 
 
+
+## BufferedReader reader = new BufferedReader(new FileReader("example.txt")
+- FileReader reads character by character → Less efficient for large files.
+- BufferedReader reads chunks of data into a buffer (not the whole file at once) → More efficient because it reduces the number of I/O calls.
+
+    
 ## while((line = reader.readLine()) != null)
 - Purpose: Reads the file line by line until there are no more lines.
 - readLine(): Reads one line from the file at a time.
 - != null: The loop continues as long as a line is read.
 
 - e.printStackTrace(): Prints the exception details to help identify the issue.
-
