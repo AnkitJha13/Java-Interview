964d2affdd14b8258f642a2e670a8e3ff7f9bf9a
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-21 21:43:49 +0530

    Update SortExample.java

--- comparable/SortExample.java
+++ comparable/SortExample.java
@@ -1,26 +1,24 @@
-package comparable;
+package ComparablePractice;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class SortExample {
-        public static void main(String[] args) {
-            ArrayList<Emp> emps = new ArrayList<>();
-            emps.add(new Emp("athena", "8272837383", 2)); 
-            emps.add(new Emp("mamba", "7272835433", 1));
-            emps.add(new Emp("levinho", "9272837383", 3));
+    public static void main(String[] args) {
+        ArrayList<Emp> emps = new ArrayList<>();
+        emps.add(new Emp("athena", "8272837383", 2));
+        emps.add(new Emp("levinho", "8474840191", 1));
+        emps.add(new Emp("mamba", "6293729383", 4));
+        emps.add(new Emp("mortal", "029389202", 3));
 
-            System.out.println(emps);
+       // System.out.println("emps sorted are " + emps);
 
-            Collections.sort(emps);
+        Collections.sort(emps);
 
-            System.out.println(emps + " after sorting");
-
-
-        }
+        System.out.println("emps details after sorting are " + emps);
+    }
 }
 
-// Output
-- 1  (sorting based on ID's)
-- 2
-- 3
+// Output 
+
+- based on ID (for Descending order)
